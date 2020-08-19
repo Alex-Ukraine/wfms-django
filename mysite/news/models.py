bf4c682a460338adfdf9b8e42267faa1a827345b
Alex-Ukraine <win.city18@gmail.com>
2020-08-19 17:10:52 +0300

    starting 9 lesson

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True) # Не обязательно к заполнению
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)  # изменение при редактировании
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
