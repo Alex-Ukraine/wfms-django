b284fa91bb9796ff308916157673b07e91142853
Alex-Ukraine <win.city18@gmail.com>
2020-08-19 23:51:00 +0300

    starting lesson 11

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=150)
-    content = models.TextField(blank=True) # Не обязательно к заполнению
+    content = models.TextField(blank=True)  # Не обязательно к заполнению
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  # изменение при редактировании
     photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello World!')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 
 def test(request):
